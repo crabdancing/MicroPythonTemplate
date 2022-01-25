819ad7fd054bfe0a5d58e7ee4ad2cf760a54d455
Alexandria <alxpettit@gmail.com>
2022-01-25 15:44:55 -0800

    now suggests specific ACM port options

--- utils/check-port.sh
+++ utils/check-port.sh
@@ -10,6 +10,9 @@ touch "${PORT}" || {
   echo "Port check failed!"
   echo "- You may want to replug your device."
   echo "- Confirm that your configured port, (${PORT}), is correct. "
+  echo "Available ACM ports include: $(ls /dev/ttyACM*)"
   echo "(You can change this setting in 'project.conf')"
   exit 1
+} && {
+  echo "Using port: ${PORT}"
 }
